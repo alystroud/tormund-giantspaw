a70ca06eb3b995565c8e7a966ab4d3ec8d185167
alystroud <alygrace@hotmail.co.uk>
2019-08-28 18:39:15 +0100

    Adding nav and footer - first implementation

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => (
+  <div className="nav">
+    <ul>
+      <li>Tormund Giantspaw</li>
+      <li>Home</li>
+      <li>Gallery</li>
+    </ul>
+  </div>
+);
+
+export default Nav;
