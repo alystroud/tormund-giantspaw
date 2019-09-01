bdf91f210b029d9be7ead8069cc5b083641e9639
alystroud <alygrace@hotmail.co.uk>
2019-09-01 19:09:24 +0100

    Add tags to gallery card

--- src/GalleryCard.js
+++ src/GalleryCard.js
@@ -12,11 +12,17 @@ class GalleryCard extends React.Component {
   }
 
   render() {
+    const tags = (this.state.tags).map((tag) =>
+      <div key={tag} className="gallery-tag">
+        <a href="">{'#' + tag + '  '}</a>
+      </div>
+    );
+
     return (
       <div className="card">
-        <img src={this.state.path} className="image"/>
+        <img src={this.state.path} className="image" alt={this.state.caption}/>
         <div>{this.state.caption}</div>
-        <div>{this.state.tags}</div>
+        <div className="gallery-tags">{tags}</div>
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -6,9 +6,9 @@ const Main = () => (
     <img  src="./images/20190623_1809.jpg"
           className="profile"
           alt="logo" />
-    <p>
-      Hi I'm Tormund Giantspaw!
-    </p>
+    <p>Hi I'm Tormund Giantspaw!</p>
+    <p>I like chewing hair ties and jumping out to scare people.</p>
+    <p>My favourite snacks are chicken and sweetcorn.</p>
   </div>
 );
 
