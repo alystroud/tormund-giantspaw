9354dc2b328a64884acbd63d0fc909dcb682cc2a
alystroud <alygrace@hotmail.co.uk>
2020-05-17 11:03:44 +0100

    Improve wording on front page. Updae ReadMe

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # Tormund Giantspaw
 A small project about my cat Tormund :heart_eyes_cat: :yellow_heart:
 ## Languages
-This project is a front-end project written with React.js, HTML and CSS.
+This project is a front-end project written with React.js, Node.js, HTML and CSS.
+
+## Getting started
+### Prerequisites
+- Node
+
+### Clone project
+1. `git clone https://github.com/alystroud/tormund-giantspaw.git`
+
+###Install dependencies
+`npm install`
+
+###Build and Run Project
+`npm run dev`
+
+##Project Status
+This project is still in development.
--- src/Main.js
+++ src/Main.js
@@ -14,20 +14,29 @@ class Main extends React.Component {
     return (
       <div className="main">
         <h1 className="title">Tormund Giantspaw!</h1>
-        <div className="row">
-          <div className="col-sm-12 col-xs-12 col-md-8 col-lg-8  order-1.order-sm-2.order-xs-2 text">
-            <p>I am {years} year{years === 1 ? "" : 's'} old.</p>
-            <p>I like chewing hair ties and jumping out to scare people.</p>
-            <p>My favourite snacks are chicken and sweetcorn.</p>
-            <p>And yes, I'm named after the
-              <a className="tormund-link" href="https://gameofthrones.fandom.com/wiki.Tormund"> ginger hunk </a>
-              from Game of Thrones!
-            </p>
-          </div>
-          <div className="col-sm-12 col-xs-12 col-md-4 col-lg-4 .order-first.order-sm-1.order-xs-1">
+        <div className="row mainRow">
+          <div className="col-sm-12 col-xs-12 col-md-4 col-lg-6 col-xl-6">
             <img src="images/20190623_1809.jpg"
                  className="profile"
                  alt="logo" />
+          </div>
+          <div className="col-sm-12 col-xs-12 col-md-8 col-lg-6 col-xl-6 text">
+            <p>I'm Tormund. I am {years} year{years === 1 ? "" : 's'} old ginger tom.</p>
+            <p>My mum named me after some hunky
+              <a className="tormund-link" href="https://gameofthrones.fandom.com/wiki.Tormund"> ginger hunk </a>
+              off the telly. Although I'm a little chunky, I'm a real softie.
+              I love a cuddle and a scritchy-scratch, especially in that magic under-chin spot.</p>
+            <p>If you feed me tuna...ahh..tuna... Sorry what was I saying?! Oh yes, I will be your
+              best friend if you feed me tuna or sweetcorn. But be careful, I do bite!
+              So mind your fingers!
+            </p>
+            <p>I hate closed doors, I like to be able to see you at all times. So if you
+              thought you'd be able to wee in peace, think again! And have a nice relaxing bath on your own?
+              Well as long as you can relax with the sound of me crying outside the door.
+            </p>
+            <p>I'm too young to have my own Instagram, so my mum made me my own website to
+              show off all the purr-fect photos of me.
+            </p>
           </div>
         </div>
       </div>
